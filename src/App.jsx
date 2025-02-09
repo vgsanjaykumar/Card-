c6ca3dfefd7d290d23a6efbaee4e2e7cf73540bb
vgsanjaykumar <143058056+vgsanjaykumar@users.noreply.github.com>
2025-02-09 13:44:13 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import Skills from './comp/Skills'
+
+const App = () => {
+  return (
+    <div>
+         <Skills/>
+    </div>
+  )
+}
+
+export default App
